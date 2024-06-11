b3ec4adee95476a663306e2b69d1241abf56b3a2
Muhammad-Shariq365 <108149127+Muhammad-Shariq365@users.noreply.github.com>
2024-06-12 03:04:37 +0500

    Update Team.js

--- law-firm/app/components/Team.js
+++ law-firm/app/components/Team.js
@@ -9,7 +9,7 @@ const Team = () => {
       <div className='container mx-auto text-center'>
         <h2 className='text-5xl font-primary font-extrabold mb-4 pt-20'>Our Attorneys</h2>
         <p className='max-w-[540px] mx-auto px-6 lg:px-0 mb-[60px]'>
-          "Our attorney section features a team of seasoned experts dedicated to delivering tailored counsel across various practice areas. Trust our track record of success and commitment to client satisfaction for all your legal needs."
+          &quot;Our attorney section features a team of seasoned experts dedicated to delivering tailored counsel across various practice areas. Trust our track record of success and commitment to client satisfaction for all your legal needs.&quot;
         </p>
         <div className='lg:grid lg:grid-cols-3 lg:gap-x-[30px]'>
           {images.map((item, index) => ( // Mapping over the images array
@@ -17,7 +17,7 @@ const Team = () => {
               {/* Using the Next.js Image component */}
               <Image src={item.src} alt={item.alt} width={300} height={300} className="mx-auto" />
               <h4 className='text-2xl mb-2 font-medium'>
-              {item.name}
+                {item.name}
               </h4>
               <p className='text-sm uppercase tracking-[0.3px] mb-4 opacity-[0.8]'>
                 {item.position}
